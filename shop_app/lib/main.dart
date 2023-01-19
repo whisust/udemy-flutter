04bf38df5e03f9b47db8a07e54c867370396ff0f
Antoine Latrille <antoine.latrille@gmail.com>
2023-01-19 23:52:59 +0100

    Start the Shop app up to lesson 193

---
+++ shop_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'screens/product_detail_screen.dart';
+import 'screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple, accentColor: Colors.deepOrange),
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+      routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
+    );
+  }
+}
---
+++ shop_app/lib/widgets/product_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+          child: GestureDetector(
+            child: Image.network(product.imageUrl, fit: BoxFit.cover),
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+            },
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            subtitle: Text(product.description),
+          )),
+    );
+  }
+}
