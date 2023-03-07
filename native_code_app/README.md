117a44db0ee2db85f35565382ab74b075c7fbcb0
Antoine Latrille <antoine.latrille@gmail.com>
2023-03-07 12:07:34 +0100

    Continue and finish the Native Code App up to lesson 364

--- native_code_app/README.md
+++ native_code_app/README.md
@@ -1,16 +1,14 @@
-# native_code_app
+# Native Code App
 
-A new Flutter project.
+Une app stupide pour apprendre à appeler du code natif iOS / Android depuis Flutter
 
-## Getting Started
+## Sujets Abordés
 
-This project is a starting point for a Flutter application.
+### Flutter
+- MethodChannel pour appeler une méthode native
 
-A few resources to get you started if this is your first Flutter project:
+### Android Java
+- MethodChannel et `call`/ `result`
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Obj-C iOS
+- 🤢
--- native_code_app/ios/Runner/AppDelegate.m
+++ native_code_app/ios/Runner/AppDelegate.m
@@ -1,13 +1,48 @@
 #import "AppDelegate.h"
+#import <Flutter/Flutter.h>
 #import "GeneratedPluginRegistrant.h"
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+
+    FlutterMethodChannel* batteryChannel = [FlutterMethodChannel
+                                            methodChannelWithName:@"dev.native_code_app/battery"
+                                            binaryMessenger:controller.binaryMessenger];
+    __weak typeof(self) weakSelf = self;
+    
+    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        if([@"getBatteryLevel" isEqualToString:call.method]){
+            int batteryLevel = [weakSelf getBatteryLevel];
+            if(batteryLevel == -1) {
+                result(@(-10));
+//                result([FlutterError errorWithCode:@"UNAVAILABLE"
+//                                           message:@"Battery Level not available"
+//                                           details:nil]);
+            } else {
+                result(@(batteryLevel));
+            }
+        } else {
+            result(FlutterMethodNotImplemented);
+        }
+      }];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+
+- (int)getBatteryLevel {
+    UIDevice* device = UIDevice.currentDevice;
+    device.batteryMonitoringEnabled = YES;
+    if(device.batteryState == UIDeviceBatteryStateUnknown) {
+        return -1;
+    } else {
+        return (int)(device.batteryLevel * 100);
+    }
 }
 
 @end
